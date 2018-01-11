0b8bf66a027279d8464e2f62620dba7ca8ca74c6
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 20:39:57 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,15 @@ ActiveRecord::Schema.define(version: 20140922151507) do
     t.integer "pond_id"
   end
 
+  create_table "ponds", force: :cascade do |t|
+    t.string "name"
+    t.string "water_type"
+  end
+
+  create_table "tadpoles", force: :cascade do |t|
+    t.string  "name"
+    t.string  "color"
+    t.integer "frog_id"
+  end
+
 end
